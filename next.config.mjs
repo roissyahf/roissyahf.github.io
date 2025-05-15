bacfc4b3a6b5d23a1336e94e62e97b670f7f3a1f
roissyahf <roissyahfk@gmail.com>
2025-05-15 20:52:28 +0700

    edit config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -8,7 +9,7 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
-  },
+  }
 }
 
 export default nextConfig
